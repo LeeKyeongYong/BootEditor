066d72197ce07ac774aa3624fa4871246e33bfbf
이경용 <sleekydz86@icloud.com>
2024-01-01 01:22:12 +0900

    스프링부트 CkEditor버전 업데이트
    스프링부트 CkEditor버전 업데이트

---
+++ src/main/java/com/beditor/ckeditor/CkeditorApplication.java
@@ -0,0 +1,13 @@
+package com.beditor.ckeditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CkeditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CkeditorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/beditor/ckeditor/CkeditorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beditor.ckeditor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CkeditorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
